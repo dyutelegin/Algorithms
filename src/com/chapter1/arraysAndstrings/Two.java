c569236fa8b691a68af7893f4aa34bc21179130a
Dmitrii Telegin <46483682+dyutelegin@users.noreply.github.com>
2019-04-20 16:22:51 +0300

    Add files via upload

---
+++ src/com/chapter1/arraysAndstrings/Two.java
@@ -0,0 +1,21 @@
+package com.chapter1.arraysAndstrings;
+import java.util.Arrays;
+
+public class Two {
+    private static String sort(String s) {
+        char[] string = s.toCharArray();
+        Arrays.sort(string);
+        return new String(string);
+    }
+
+    private static boolean permutation (String a,String b) {
+        if (a.length() != b.length()) {
+            return false;
+        }
+        return sort(a).equals(sort(b));
+    }
+    public static void main(String[] args){
+        System.out.println(permutation("test1", "test_1"));
+        System.out.println(permutation("test", "test"));
+    }
+}
