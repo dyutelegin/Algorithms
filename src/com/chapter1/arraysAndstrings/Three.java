3bdb7fb619fa545e67bb506908a2cb8ed3d47138
Dmitrii Telegin <46483682+dyutelegin@users.noreply.github.com>
2019-04-20 16:37:22 +0300

    Add files via upload

---
+++ src/com/chapter1/arraysAndstrings/Three.java
@@ -0,0 +1,11 @@
+package com.chapter1.arraysAndstrings;
+
+public class Three {
+    private static String space(String s){
+        s = s.replaceAll("\\s+", "%20");
+        return s;
+    }
+    public static void main(String[] args){
+        System.out.println(space("One two three "));
+    }
+}
