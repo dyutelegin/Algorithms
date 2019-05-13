007ecfd18c2bc82cc3002bdf0a838a1b6b465d73
dyutelegin <dyutelegin@gmail.com>
2019-05-13 13:22:13 +0300

    Add 1 submission in Chapter 3

--- src/com/chapter3/StacksAndQueues/One.java
+++ src/com/chapter3/StacksAndQueues/One.java
@@ -1,5 +1,36 @@
 package com.chapter3.StacksAndQueues;
 
 public class One {
+    int stackSize = 100;
+    int[] buffer = new int [stackSize * 3];
+    int[] stackPointer = {0,0,0};
 
+    void push(int stackNum, int value) throws Exception {
+        if (stackPointer[stackNum] >= stackSize){
+            throw new Exception("Not enough space");
+        }
+        int index = stackNum * stackSize + stackPointer[stackNum] + 1;
+        stackPointer[stackNum]++;
+        buffer[index] = value;
+    }
+
+    int pop(int stackNum) throws Exception {
+        if (stackPointer[stackNum] == 0) {
+            throw new Exception("Attempt to use empty stack");
+        }
+        int index = stackNum * stackSize + stackPointer[stackNum];
+        stackPointer[stackNum]--;
+        int value = buffer[index];
+        buffer[index] = 0;
+        return value;
+    }
+
+    int peek(int stackNum) {
+        int index = stackNum * stackSize + stackPointer[stackNum];
+        return buffer[index];
+    }
+
+    boolean isEmpty(int stackNum) {
+        return stackPointer[stackNum] ==0;
+    }
 }
